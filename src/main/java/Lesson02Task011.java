6e8829477bae1f7ea35a26a2f74da80fa28c9e56
PSV <pavshin@mail.ru>
2022-06-21 22:09:29 +0700

    Поиск числа в массиве - решено

--- src/main/java/Lesson02Task011.java
+++ src/main/java/Lesson02Task011.java
@@ -7,45 +7,45 @@ public class Lesson02Task011 {
     public static void main(String[] args) {
         int n = 10;
         int[] arr = new int[n];
-        for (int i=0; i<arr.length; i++) {
-            arr[i]=new Random().nextInt(100);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Random().nextInt(100);
         }
         int indexToFind = 3;
 
         System.out.println("Массив заполненный случайными числами:");
-        for (int el: arr){
-            System.out.print(el+" ");
+        for (int el : arr) {
+            System.out.print(el + " ");
         }
 
         int toFind = arr[indexToFind];
-        System.out.println("\nИскомое число: "+toFind);
+        System.out.println("\nИскомое число: " + toFind);
 
         Arrays.sort(arr);
         System.out.println("Отсортированный массив:");
-        for (int el: arr){
-            System.out.print(el+" ");
+        for (int el : arr) {
+            System.out.print(el + " ");
         }
 
-        int min=0;
-        int max=arr.length-1;
+        int min = 0;
+        int max = arr.length - 1;
         int index = 1;
-        if(arr[min]==toFind){
-            indexToFind=min;
-        } else if (arr[max]==toFind) {
-            indexToFind=max;
+        if (arr[min] == toFind) {
+            indexToFind = min;
+        } else if (arr[max] == toFind) {
+            indexToFind = max;
         } else {
-            while (arr[indexToFind]!=toFind) {
+            while (arr[indexToFind] != toFind) {
                 if (toFind > arr[indexToFind]) {
-                    min=indexToFind;
-                    indexToFind=(max+indexToFind)/2;
+                    min = indexToFind;
+                    indexToFind = (max + indexToFind) / 2;
                 } else {
-                    max=indexToFind;
-                    indexToFind=(min+indexToFind)/2;
+                    max = indexToFind;
+                    indexToFind = (min + indexToFind) / 2;
                 }
                 index++;
             }
         }
-        System.out.printf("\nИскомое значение %d находится на %d индексе.\nПотребовалос %d итераций",toFind,indexToFind,index);
+        System.out.printf("\nИскомое значение %d находится на %d индексе.\nПотребовалос %d итераций", toFind, indexToFind, index);
 
     }
 
