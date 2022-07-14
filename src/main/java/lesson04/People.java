4d7f53081f6045e0e6f296096e9a45a5931f4d06
PSV <pavshin@mail.ru>
2022-07-14 09:31:20 +0700

    -

--- src/main/java/lesson04/People.java
+++ src/main/java/lesson04/People.java
@@ -4,45 +4,32 @@ public class People {
     private String firstName;
     private String secondName;
     private int strength;
-    private int dexterity;
     private int speed;
 
-    //public People(){ //String fName, String sName, int str, int dex, int speed
-    public void setFirstName(String fName){
-        this.firstName=fName;
-    }
-    public void setSecondName(String sName){
-        this.secondName=sName;
-    }
-
-    public void setStrength(int str){
-        this.strength=str;
-    }
-    public void setDexterity(int dex){
-        this.dexterity=dex;
+    public People(String fName, String sName, int strength, int speed) {
+        this.firstName = fName;
+        this.secondName = sName;
+        this.strength = strength;
+        this.speed = speed;
     }
 
-    public void setSpeed(int speed){
-        this.speed=speed;
-    }
-
-    public String getFirstName(){
+    public String getFirstName() {
         return this.firstName;
     }
 
-    public String getSecondName(){
+    public String getSecondName() {
         return this.secondName;
     }
 
-    public int getStrength(){
+    public int getStrength() {
         return this.strength;
     }
 
-    public int getDexterity(){
-        return this.dexterity;
+    public int getSpeed() {
+        return this.speed;
     }
 
-    public int getSpeed(){
-        return this.speed;
+    public void print(){
+        System.out.println(this.firstName+", "+this.secondName+", strength="+this.strength+", speed="+this.speed);
     }
 }
--- src/main/java/lesson04/competition.java
+++ src/main/java/lesson04/competition.java
@@ -1,27 +1,27 @@
 package lesson04;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class competition {
     public static void main(String[] args){
-        //"Pavshin","Sergey",5,5,5
-        //Team t = new Team();
-        //t.f=44;
-        Obstacal ob1 = new Obstacal();
-        ob1.f=54;
-        ob1.str="dkj";
-        System.out.println(ob1.f);
+        int players = 10;
+        List<People> gamers = new ArrayList<>(players);
+        String[] familys = new String[]{"Pavshin","Jashin","Fetisov","Ozerov","Ivanov","Petrov","Koshkin","Mamin","Olgin","Itogin"};
+        String[] names = new String[]{"Sergey","Oleg","Vitally","Ivan","Maxim","Petr","Alexey","Evgeny","Andrey","Dmitry"};
+        for (int i =0;i<players;i++){
+            int fIndex = new Random().nextInt(familys.length);
+            int sIndex = new Random().nextInt(names.length);
+            int str = new Random().nextInt(10)+1;
+            int sp = new Random().nextInt(10)+1;
+            People gamer = new People(familys[fIndex],names[sIndex],str,sp);
+            gamers.add(i,gamer);
+        }
+        for (People el: gamers) {
+            el.print();
+        }
 
-        //People gamer1 = new People();
-        //System.out.println(gamer1.getFirstName());
-        //System.out.println(gamer1.getSecondName());
-//        System.out.println(gamer1.getStrength());
-//        System.out.println(gamer1.getDexterity());
-//        System.out.println(gamer1.getSpeed());
+
     }
-
-    public static class Obstacal {
-        int f;
-        String str;
-    }
-
-
 }
